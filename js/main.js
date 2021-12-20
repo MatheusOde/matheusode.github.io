ee9a6a60ef25034ae1104128eb375be60029ffdc
matheus <matheusode@gmail.com>
2021-12-20 18:33:00 -0300

    scroll feature sucessfully implemented BABYgit add -A!

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ mobileBtn.addEventListener("click", () => {
 mobileBtnExit.addEventListener("click", () => {
     nav.classList.remove("menu-btn");
 });
+
+let scrollTop = 0;
+const firstIntro = document.querySelector('.first-introduction');
+const personalInfo = document.querySelector('.little-story');
+
+window.addEventListener('scroll', (e) => {
+    scrollTop = window.pageYOffset;
+    firstIntroHeight = firstIntro.offsetHeight;
+    personalInfoHeight = personalInfo.offsetHeight;
+    firstIntro.style.opacity = 1 - scrollTop / firstIntroHeight * 0.5;
+    personalInfo.style.opacity = 1 - (scrollTop - firstIntroHeight * 2) / personalInfoHeight * 0.5;
+});
