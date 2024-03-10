92b85c0b61ec88cf3e79d14cd882319d36a415da
MatheusOde <matheusode@gmail.com>
2024-03-10 13:23:44 -0300

    Added contact information button functionality

--- js/main.js
+++ js/main.js
@@ -14,6 +14,21 @@ projects.addEventListener('click', () => {
 
 const skills = document.getElementById('skills-btn');
 skills.addEventListener('click', () => {
+        console.log("shite");
         document.getElementById("skill").scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'end' });
 
+});
+
+function openPopup() {
+  document.getElementById("contact-popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("contact-popup").style.display = "none";
+}
+
+const contact = document.getElementById('contact-btn');
+skills.addEventListener('click', () => {
+        console.log("shit");
+        openPopup();
 });
